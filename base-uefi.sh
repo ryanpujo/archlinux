f7e64f0fbebdc3a944950a58a693f27a6d6c9aa2
Pujo Triantoro <pujotriantoro@gmail.com>
2026-01-26 07:31:49 +0700

    replace tlp to power-profiles-daemon

--- base-uefi.sh
+++ base-uefi.sh
@@ -16,14 +16,14 @@ echo root:password | chpasswd
 
 echo "ParallelDownloads = 5" >> /etc/pacman.conf
 
-pacman -S grub grub-btrfs efibootmgr cmake ninja clang networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers linux-atm avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+# uncomment these 2 lines to enable multilib repository
+echo "[multilib]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+
+pacman -Syu grub grub-btrfs efibootmgr cmake ninja clang networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers linux-atm avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call power-profiles-daemon virt-manager qemu edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
 # pacman -S --noconfirm xf86-video-amdgpu
 pacman -S --noconfirm nvidia-open nvidia-utils nvidia-settings
-
-# uncomment these 2 lines to enable multilib repository
- echo "[multilib]" >> /etc/pacman.conf
- echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
 
@@ -34,7 +34,7 @@ systemctl enable bluetooth
 systemctl enable cups.service
 systemctl enable sshd
 systemctl enable avahi-daemon
-systemctl enable tlp
+systemctl enable power-profiles-daemon.service
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable libvirtd
